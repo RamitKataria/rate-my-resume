9b2802d68db0974915c0a462ef6ff9f2c2c1c16d
Ramit Kataria <ramit.kataria@gmail.com>
2023-10-14 19:28:25 -0700

    Implement basic API

---
+++ server/routes/index.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  res.status(200).send('index', {
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+});
+
+router.get('/user', requiresAuth(), function (req, res, next) {
+  console.log(req.oidc.user);
+  const user = req.oidc.user;
+  res.status(200).send({
+    picture: user.picture,
+    userID: user.sub
+  });
+});
+
+module.exports = router;
