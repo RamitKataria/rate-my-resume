5100e5e648dfc8bc449dc91b75f3a83c9cfdde1c
Candyzorua <jiaying.ong2@gmail.com>
2023-10-15 10:46:27 -0700

    Minor changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Title } from '@angular/platform-browser';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'CoreUI Free Angular Admin Template';
+  title = 'Rate My Resume';
 
   constructor(
     private router: Router,
